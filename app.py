f9b533ce1605fad3aca3b523c071931eb9a65778
Taejoon <82999550+tjmoon-vntg@users.noreply.github.com>
2025-08-06 17:18:07 +0900

    Update app.py Hello world 3

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World - 2!"
+    return "Hello, World - 3!"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
